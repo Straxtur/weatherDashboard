8696e90d44171a970b3b05f7581235bda6f9eb4f
Jorge Sánchez López <jorgeraton70@gmail.com>
2025-03-05 19:19:29 +0100

    Configurar Prettier y ESLint, ajustar .gitignore y mejorar la estructura del código

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,37 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import neostandard from 'neostandard'
 
-export default tseslint.config(
+export default [
+  ...neostandard(),
   { ignores: ['dist'] },
+
+  // Configuración unificada para React + TypeScript
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-)
+]
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 const App = () => {
-
-    return <h1>Hola mundo</h1>
+  const p = 'sds'
+  return <h1>Hola mundo</h1>
 }
-
 
 export default App
