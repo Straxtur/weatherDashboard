a9a025cc4cab4657c7cbc2de117d2e6d3963fe40
Jorge Sánchez López <jorgeraton70@gmail.com>
2025-03-25 12:54:41 +0100

    Agregar soporte para Vercel Analytics: instalar el paquete y renderizar el componente en la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { Analytics } from "@vercel/analytics/react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
 import { CurrentWeatherProvider } from "./context/WeatherContext"
@@ -11,7 +12,8 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <CurrentWeatherProvider>
       <App />
+      <Analytics />
     </CurrentWeatherProvider>
-    <ReactQueryDevtools initialIsOpen={false} />
+    {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
   </QueryClientProvider>
 )
